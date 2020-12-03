57da017f2116156141fd6df6487d365a4a38b1b6
anpai057 <75323741+anpai057@users.noreply.github.com>
2020-12-03 08:08:26 -0700

    Add files via upload
    1st upload

---
+++ WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,330 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox16wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox17wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox14wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox13wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox15wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox12wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox11wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox10wall = New System.Windows.Forms.PictureBox()
+        Me.bullet = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4wall = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.pictureWIN = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox16wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox17wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox14wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox13wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox15wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox12wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox11wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.bullet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4wall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pictureWIN, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        '
+        'Timer3
+        '
+        Me.Timer3.Enabled = True
+        Me.Timer3.Interval = 20
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(59, 415)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(100, 23)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.WindowsApp1.My.Resources.Resources.maxresdefault__1_
+        Me.PictureBox3.Location = New System.Drawing.Point(732, 409)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(59, 28)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 18
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox16wall
+        '
+        Me.PictureBox16wall.BackColor = System.Drawing.Color.Black
+        Me.PictureBox16wall.Location = New System.Drawing.Point(764, 179)
+        Me.PictureBox16wall.Name = "PictureBox16wall"
+        Me.PictureBox16wall.Size = New System.Drawing.Size(10, 144)
+        Me.PictureBox16wall.TabIndex = 17
+        Me.PictureBox16wall.TabStop = False
+        '
+        'PictureBox17wall
+        '
+        Me.PictureBox17wall.BackColor = System.Drawing.Color.Black
+        Me.PictureBox17wall.Location = New System.Drawing.Point(665, 205)
+        Me.PictureBox17wall.Name = "PictureBox17wall"
+        Me.PictureBox17wall.Size = New System.Drawing.Size(14, 140)
+        Me.PictureBox17wall.TabIndex = 16
+        Me.PictureBox17wall.TabStop = False
+        '
+        'PictureBox14wall
+        '
+        Me.PictureBox14wall.BackColor = System.Drawing.Color.Black
+        Me.PictureBox14wall.Location = New System.Drawing.Point(487, -19)
+        Me.PictureBox14wall.Name = "PictureBox14wall"
+        Me.PictureBox14wall.Size = New System.Drawing.Size(10, 104)
+        Me.PictureBox14wall.TabIndex = 15
+        Me.PictureBox14wall.TabStop = False
+        '
+        'PictureBox13wall
+        '
+        Me.PictureBox13wall.BackColor = System.Drawing.Color.Black
+        Me.PictureBox13wall.Location = New System.Drawing.Point(427, 158)
+        Me.PictureBox13wall.Name = "PictureBox13wall"
+        Me.PictureBox13wall.Size = New System.Drawing.Size(70, 10)
+        Me.PictureBox13wall.TabIndex = 14
+        Me.PictureBox13wall.TabStop = False
+        '
+        'PictureBox15wall
+        '
+        Me.PictureBox15wall.BackColor = System.Drawing.Color.Black
+        Me.PictureBox15wall.Location = New System.Drawing.Point(253, 415)
+        Me.PictureBox15wall.Name = "PictureBox15wall"
+        Me.PictureBox15wall.Size = New System.Drawing.Size(157, 10)
+        Me.PictureBox15wall.TabIndex = 13
+        Me.PictureBox15wall.TabStop = False
+        '
+        'PictureBox12wall
+        '
+        Me.PictureBox12wall.BackColor = System.Drawing.Color.Black
+        Me.PictureBox12wall.Location = New System.Drawing.Point(274, 176)
+        Me.PictureBox12wall.Name = "PictureBox12wall"
+        Me.PictureBox12wall.Size = New System.Drawing.Size(10, 158)
+        Me.PictureBox12wall.TabIndex = 12
+        Me.PictureBox12wall.TabStop = False
+        '
+        'PictureBox11wall
+        '
+        Me.PictureBox11wall.BackColor = System.Drawing.Color.Black
+        Me.PictureBox11wall.Location = New System.Drawing.Point(176, 86)
+        Me.PictureBox11wall.Name = "PictureBox11wall"
+        Me.PictureBox11wall.Size = New System.Drawing.Size(69, 10)
+        Me.PictureBox11wall.TabIndex = 11
+        Me.PictureBox11wall.TabStop = False
+        '
+        'PictureBox9wall
+        '
+        Me.PictureBox9wall.BackColor = System.Drawing.Color.Black
+        Me.PictureBox9wall.Location = New System.Drawing.Point(563, 336)
+        Me.PictureBox9wall.Name = "PictureBox9wall"
+        Me.PictureBox9wall.Size = New System.Drawing.Size(117, 10)
+        Me.PictureBox9wall.TabIndex = 10
+        Me.PictureBox9wall.TabStop = False
+        '
+        'PictureBox10wall
+        '
+        Me.PictureBox10wall.BackColor = System.Drawing.Color.Black
+        Me.PictureBox10wall.Location = New System.Drawing.Point(416, 158)
+        Me.PictureBox10wall.Name = "PictureBox10wall"
+        Me.PictureBox10wall.Size = New System.Drawing.Size(12, 299)
+        Me.PictureBox10wall.TabIndex = 9
+        Me.PictureBox10wall.TabStop = False
+        '
+        'bullet
+        '
+        Me.bullet.BackColor = System.Drawing.Color.Transparent
+        Me.bullet.Image = Global.WindowsApp1.My.Resources.Resources._4
+        Me.bullet.Location = New System.Drawing.Point(29, 132)
+        Me.bullet.Name = "bullet"
+        Me.bullet.Size = New System.Drawing.Size(47, 38)
+        Me.bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.bullet.TabIndex = 8
+        Me.bullet.TabStop = False
+        Me.bullet.Visible = False
+        '
+        'PictureBox7wall
+        '
+        Me.PictureBox7wall.BackColor = System.Drawing.Color.Black
+        Me.PictureBox7wall.Location = New System.Drawing.Point(580, 12)
+        Me.PictureBox7wall.Name = "PictureBox7wall"
+        Me.PictureBox7wall.Size = New System.Drawing.Size(10, 340)
+        Me.PictureBox7wall.TabIndex = 7
+        Me.PictureBox7wall.TabStop = False
+        '
+        'PictureBox6wall
+        '
+        Me.PictureBox6wall.BackColor = System.Drawing.Color.Black
+        Me.PictureBox6wall.Location = New System.Drawing.Point(324, 12)
+        Me.PictureBox6wall.Name = "PictureBox6wall"
+        Me.PictureBox6wall.Size = New System.Drawing.Size(10, 261)
+        Me.PictureBox6wall.TabIndex = 6
+        Me.PictureBox6wall.TabStop = False
+        '
+        'PictureBox5wall
+        '
+        Me.PictureBox5wall.BackColor = System.Drawing.Color.Black
+        Me.PictureBox5wall.Location = New System.Drawing.Point(176, 86)
+        Me.PictureBox5wall.Name = "PictureBox5wall"
+        Me.PictureBox5wall.Size = New System.Drawing.Size(10, 362)
+        Me.PictureBox5wall.TabIndex = 5
+        Me.PictureBox5wall.TabStop = False
+        '
+        'PictureBox4wall
+        '
+        Me.PictureBox4wall.BackColor = System.Drawing.Color.Black
+        Me.PictureBox4wall.Location = New System.Drawing.Point(82, 2)
+        Me.PictureBox4wall.Name = "PictureBox4wall"
+        Me.PictureBox4wall.Size = New System.Drawing.Size(10, 321)
+        Me.PictureBox4wall.TabIndex = 4
+        Me.PictureBox4wall.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Black
+        Me.PictureBox1.Image = Global.WindowsApp1.My.Resources.Resources.maxresdefault
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(76, 51)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'pictureWIN
+        '
+        Me.pictureWIN.BackColor = System.Drawing.Color.Transparent
+        Me.pictureWIN.Image = Global.WindowsApp1.My.Resources.Resources.unnamed2
+        Me.pictureWIN.Location = New System.Drawing.Point(652, 34)
+        Me.pictureWIN.Name = "pictureWIN"
+        Me.pictureWIN.Size = New System.Drawing.Size(78, 77)
+        Me.pictureWIN.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pictureWIN.TabIndex = 3
+        Me.pictureWIN.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.WindowsApp1.My.Resources.Resources._6beccf67372a433538bb9b28867ee89f
+        Me.PictureBox2.Location = New System.Drawing.Point(-9, 2)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(816, 455)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.PictureBox16wall)
+        Me.Controls.Add(Me.PictureBox17wall)
+        Me.Controls.Add(Me.PictureBox14wall)
+        Me.Controls.Add(Me.PictureBox13wall)
+        Me.Controls.Add(Me.PictureBox15wall)
+        Me.Controls.Add(Me.PictureBox12wall)
+        Me.Controls.Add(Me.PictureBox11wall)
+        Me.Controls.Add(Me.PictureBox9wall)
+        Me.Controls.Add(Me.PictureBox10wall)
+        Me.Controls.Add(Me.bullet)
+        Me.Controls.Add(Me.PictureBox7wall)
+        Me.Controls.Add(Me.PictureBox6wall)
+        Me.Controls.Add(Me.PictureBox5wall)
+        Me.Controls.Add(Me.PictureBox4wall)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.pictureWIN)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.Text = "kermy attack"
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox16wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox17wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox14wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox13wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox15wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox12wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox11wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.bullet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4wall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pictureWIN, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents pictureWIN As PictureBox
+    Friend WithEvents PictureBox4wall As PictureBox
+    Friend WithEvents PictureBox5wall As PictureBox
+    Friend WithEvents PictureBox6wall As PictureBox
+    Friend WithEvents PictureBox7wall As PictureBox
+    Friend WithEvents bullet As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents PictureBox10wall As PictureBox
+    Friend WithEvents PictureBox9wall As PictureBox
+    Friend WithEvents PictureBox11wall As PictureBox
+    Friend WithEvents PictureBox12wall As PictureBox
+    Friend WithEvents PictureBox15wall As PictureBox
+    Friend WithEvents PictureBox13wall As PictureBox
+    Friend WithEvents PictureBox14wall As PictureBox
+    Friend WithEvents PictureBox17wall As PictureBox
+    Friend WithEvents PictureBox16wall As PictureBox
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents PictureBox3 As PictureBox
+End Class
---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,143 @@
+﻿Public Class Form1
+    Dim r As New Random
+    Dim score As Integer
+
+    Sub randmove(p As PictureBox)
+        Dim x As Integer
+        Dim y As Integer
+        x = r.Next(-10, 11)
+        y = r.Next(-10, 11)
+        MoveTo(p, x, y)
+    End Sub
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        Select Case e.KeyCode
+            Case Keys.R
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipX)
+                Me.Refresh()
+            Case Keys.Up, Keys.W
+                MoveTo(PictureBox1, 0, -5)
+            Case Keys.Down, Keys.S
+                MoveTo(PictureBox1, 0, +5)
+            Case Keys.Left, Keys.A
+                MoveTo(PictureBox1, -5, 0)
+            Case Keys.Right, Keys.D
+                MoveTo(PictureBox1, 5, 0)
+            Case Keys.Space
+                bullet.Location = pictureWIN.Location
+                bullet.Visible = True
+            Case Keys.Space
+                Timer2.Enabled = True
+                bullet.Visible = True
+                bullet.Location = pictureWIN.Location
+
+        End Select
+    End Sub
+    Sub follow(p As PictureBox)
+        Static headstart As Integer
+        Static c As New Collection
+        c.Add(PictureBox1.Location)
+        headstart = headstart + 1
+        If headstart > 10 Then
+            p.Location = c.Item(1)
+            c.Remove(1)
+        End If
+    End Sub
+
+    Public Sub chase(p As PictureBox)
+        Dim x, y As Integer
+        If p.Location.X > pictureWIN.Location.X Then
+            x = -5
+        Else
+            x = 5
+        End If
+        MoveTo(p, x, 0)
+        If p.Location.Y < PictureBox1.Location.Y Then
+            y = 5
+        Else
+            y = -5
+        End If
+        MoveTo(p, x, y)
+    End Sub
+
+
+
+    Function Collision(p As PictureBox, t As String, Optional ByRef other As Object = vbNull)
+        Dim col As Boolean
+        score = score + 1
+        For Each c In Controls
+            Dim obj As Control
+            obj = c
+            If obj.Visible AndAlso p.Bounds.IntersectsWith(obj.Bounds) And obj.Name.ToUpper.Contains(t.ToUpper) Then
+                col = True
+                other = obj
+            End If
+        Next
+        Return col
+    End Function
+    'Return true or false if moving to the new location is clear of objects ending with t
+    Function IsClear(p As PictureBox, distx As Integer, disty As Integer, t As String) As Boolean
+        Dim b As Boolean
+
+        p.Location += New Point(distx, disty)
+        b = Not Collision(p, t)
+        p.Location -= New Point(distx, disty)
+        Return b
+    End Function
+    'Moves and object (won't move onto objects containing  "wall" and shows green if object ends with "win"
+    Sub MoveTo(p As PictureBox, distx As Integer, disty As Integer)
+        If IsClear(p, distx, disty, "WALL") Then
+            p.Location += New Point(distx, disty)
+        End If
+        Dim other As Object = Nothing
+        If p.Name = "PictureBox1" And Collision(p, "WIN", other) Then
+            Me.BackColor = Color.Green
+            other.visible = True
+        End If
+    End Sub
+
+
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub bullet_Click(sender As Object, e As EventArgs) Handles bullet.Click
+    End Sub
+    Dim bDir As Integer = 5
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If bullet.Location.X < 0 Then
+            bDir = 5
+        End If
+        MoveTo(bullet, 5, 0)
+
+
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox10wall.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub PictureBox6wall_Click(sender As Object, e As EventArgs) Handles PictureBox6wall.Click
+
+    End Sub
+
+    Private Sub PictureBox4wall_Click(sender As Object, e As EventArgs) Handles PictureBox4wall.Click
+
+    End Sub
+
+    Private Sub PictureBox9wall_Click(sender As Object, e As EventArgs) Handles PictureBox9wall.Click
+
+    End Sub
+
+    Private Sub PictureBox13wall_Click(sender As Object, e As EventArgs) Handles PictureBox13wall.Click
+
+    End Sub
+
+    Private Sub PictureBox14wall_Click(sender As Object, e As EventArgs) Handles PictureBox14wall.Click
+
+    End Sub
+End Class
